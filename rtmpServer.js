38c4a81c8b35974a0481cdd080d710c18dd54020
ayeletc <41619254+ayeletc@users.noreply.github.com>
2018-12-03 14:44:54 +0200

    HTTP server successfully sends data to the device when the RTMP streaming server emits events.

--- rtmpServer.js
+++ rtmpServer.js
@@ -1,12 +1,22 @@
 const RtmpServer = require('rtmp-server');
-const TcpIpServer = require('./tcpipserver');
 const rtmpServer = new RtmpServer();
+
 var kill = require('tree-kill');
-
-
 var spawn = require("child_process").spawn;
 var process = spawn('/Users/ayelet/.virtualenvs/cv/bin/python', [] );
 
+
+// Http Server for sending data to the device
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+// when this event is uncomment, the program stop here (until the client disconnects)
+// io.on('connection', function(socket) {
+// 	console.log('a user connected');
+// 	}); 
+http.listen(1937);
+
+
+// RTMP Server for video streaming
 rtmpServer.on('error', err => {
   throw err;
 });
@@ -14,7 +24,8 @@ rtmpServer.on('error', err => {
 rtmpServer.on('client', client => {
 
   client.on('connect', () => {
-     console.log('connect', client.app);
+     	console.log('connect', client.app);
+		io.emit('clientConnected', {"data" : "I got a client"});
   });
 
   client.on('play', ({ streamName }) => {
@@ -26,18 +37,17 @@ rtmpServer.on('client', client => {
     //start python process (processRTMP.py) using python from "cv" virtual env
     console.log('start python process');
     process = spawn('/Users/ayelet/.virtualenvs/cv/bin/python', ["./processRTMP.py"] );
-    console.log('spawn process id = %d\n', process.pid);
-    
-    
+//     console.log('spawn process id = %d\n', process.pid);    
     process.stdout.on('data', function(data) {
 	    console.log(data.toString());
 	    if(data.toString().includes('10')) {
 	    	console.log('match');
+
+    		io.emit('match', {"data" : 'I got a match'});
 	    }
 	    else {
 	    	console.log('no match');
-	    }
-	    
+	    }	    
     });  
     process.stderr.on('data', function(data) {
     	console.log('python error:\n' + data);
@@ -47,9 +57,7 @@ rtmpServer.on('client', client => {
   client.on('stop', () => {
     console.log('client disconnected');
     console.log('kill python process');
-    kill(process.pid);    	
-     
+//     kill(process.pid);    	
   });
 });
-
 rtmpServer.listen(1936);
